df48e36cb65fbf6b79f1cb72cdcf4ffa3d828034
gonCSgit <103418696+gonCSgit@users.noreply.github.com>
2022-08-10 00:55:11 +0100

    Delete dist directory

--- dist/users/users.service.d.ts
+++
@@ -1,8 +0,0 @@
-import { Model } from 'mongoose';
-import { User } from 'src/dto/user.dto';
-export declare class UsersService {
-    private readonly userModel;
-    constructor(userModel: Model<User>);
-    findOne(username: string): Promise<User>;
-    countUsernames(username: string): Promise<number>;
-}
