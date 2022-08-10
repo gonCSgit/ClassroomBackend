77c35227951577145e3ecdba55fa8a4238dbd224
gonCSgit <103418696+gonCSgit@users.noreply.github.com>
2022-08-10 02:00:24 +0100

    removing dist directory

--- dist/newUser/newuser.controller.d.ts
+++
@@ -1,6 +0,0 @@
-import { NewUserService } from './newuser.service';
-export declare class NewUserController {
-    private readonly appService;
-    constructor(appService: NewUserService);
-    newUser(username: string, password: string): Promise<any>;
-}
