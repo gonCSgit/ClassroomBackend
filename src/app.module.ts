94dfac948902078ae465e5b9fcb2c8d4f1613181
gonCSgit <103418696+gonCSgit@users.noreply.github.com>
2022-09-03 03:18:34 +0100

    added environment variables module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,30 @@
 import { Module } from '@nestjs/common';
-import { MongooseModule } from '@nestjs/mongoose';
+import { MongooseModule, MongooseModuleOptions } from '@nestjs/mongoose';
 import { UsersModule } from './users/users.module';
+import { AppConfigurationModule } from './app-config.module';
+import { AppConfigurationService } from './app-config.service';
 
 @Module({
   imports: [
     UsersModule,
-    MongooseModule.forRoot(
-      'mongodb+srv://dummyaccount123:qwerty4321@cluster0.s2ypjr7.mongodb.net/classroom?retryWrites=true&w=majority',
-    ),
+    AppConfigurationModule,
+    // To give this module the database’s connection string,
+    // you have to get it from the .env file and the only way to do it
+    // is by using the AppConfigurationService.
+    // Therefore you need to inject it inside the MongooseModule so you can
+    // configure the MongooseModuleOptions to use the right connection string.
+    MongooseModule.forRootAsync({
+      imports: [AppConfigurationModule],
+      inject: [AppConfigurationService],
+      useFactory: (appConfigService: AppConfigurationService) => {
+        const options: MongooseModuleOptions = {
+          uri: appConfigService.connectionString,
+          useNewUrlParser: true,
+          useUnifiedTopology: true,
+        };
+        return options;
+      },
+    }),
   ],
 })
 export class AppModule {}
