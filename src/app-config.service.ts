5e34d0d71fdaea89b6deb1b2428cf5366c07f82d
gonCSgit <103418696+gonCSgit@users.noreply.github.com>
2022-09-03 05:20:54 +0100

    Update app-config.service.ts

--- src/app-config.service.ts
+++ src/app-config.service.ts
@@ -14,7 +14,7 @@ export class AppConfigurationService {
   }
 
   private _getConnectionStringFromEnvFile(): string {
-    const connectionString = this._configService.get<string>('MONGODB_DB');
+    const connectionString = this._configService.get<string>('MONGODB_URI');
     if (!connectionString) {
       throw new Error(
         'No connection string has been provided in the .env file.',
