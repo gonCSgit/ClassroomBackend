aa989520729649adfc7f84f1241ce1217ae0a930
gonCSgit <103418696+gonCSgit@users.noreply.github.com>
2022-09-05 16:14:07 +0100

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -6,16 +6,12 @@ const cookieSession = require('cookie-session');
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors({
-    origin: true,
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
-    credentials: true,
-  });
   app.use(
     cookieSession({
       keys: ['koyaanisqatsi'],
     }),
   );
+  app.enableCors();
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
